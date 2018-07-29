558827d765eacdc3365a0551f6fcd07cf9deb1f0
WuXiaoXiong <585264@qq.com>
2018-07-30 00:31:11 +0800

    add .gitignore file

---
+++ tools/bt_parser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by xx on 2018/7/27.
+//
+
+#include "bt_parser.h"
---
+++ tools/bt_parser.h
@@ -0,0 +1,8 @@
+//
+// Created by xx on 2018/7/27.
+//
+
+#ifndef SHA1_BT_PARSER_H
+#define SHA1_BT_PARSER_H
+
+#endif //SHA1_BT_PARSER_H
